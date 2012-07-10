c2fd0abf3a184c5620390e92b18e8814fbe7fa65
Jonathan Gabel <post@jonathangabel.com>
2012-07-10 09:26:12 -0400

    code cleanup, comments standards 2.0

--- content-archive.php
+++ content-archive.php
@@ -2,24 +2,25 @@
 	/*
 	 * 	Displays the title and excerpt within the loop
 	 * 	
-	 * 	
-	 *	Called by: loop-list on non-status posts
+	 * 	CALLED BY: 	loop-archive
+	 *	CALLS TO:	inc/meta, inc/metalinks
+	 *  v 2.0	 
 	 */
 ?>
 
-<div class='excerpt clearLeft'>
-
-	<a href="<?php the_permalink(); ?>">
-	<?php 
-	if(has_post_thumbnail()) {
-		the_post_thumbnail();
-	} else {
-		$type = get_post_type( $post );
-		if ($type == 'artworks') {
-			echo '<img class="wp-post-image" src="/images/nav/artworksThumbnail.png" alt="artwork" />';
-		} else echo '<img class="wp-post-image" src="/images/nav/postThumbnail.png" alt="post" />';
-	}
-	?>
+<article class="clearLeft">
+	
+	<a href=" <?php the_permalink(); ?>">
+<?php 
+		if(has_post_thumbnail()) {
+			the_post_thumbnail();
+		} else {
+			$type = get_post_type( $post );
+			if ($type == 'artworks') {
+				echo '<img class="wp-post-image" src="/images/nav/artworksThumbnail.png" alt="artwork" />';
+			} else echo '<img class="wp-post-image" src="/images/nav/postThumbnail.png" alt="post" />';
+		}
+?>
 	</a>
 	
 	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
@@ -28,6 +29,8 @@
 	
 	<?php the_excerpt(); ?>
 
+	<span class ="archive-date"><?php include (TEMPLATEPATH . '/inc/metalinks.php' ); ?></span>
+
 	<?php edit_post_link('Edit this excerpt','',''); ?>
 
-</div>
+</article>
--- content-artworks.php
+++ content-artworks.php
@@ -2,8 +2,9 @@
 	/**
 	 * Displays 'artworks' custom-post-type content
 	 *
-	 * Called by: single.php via loop.php for custom-post-type artwork
-	 *	v1.0
+	 * 	CALLED BY: 	loop.php --> single.php for custom-post-type artwork
+	 *	CALLS TO:	
+	 *  v 2.0	 
 	 */
 ?>
 
@@ -11,20 +12,30 @@
 
 <div id="innerWrapper">	
 
-	<?php the_content(); ?>
+<?php the_content(); ?>
 	
-	<h2><?php the_title(); ?></h2>
+<h2><?php the_title(); ?></h2>
 	
 	
-	<?php  $category = get_the_category(); 
-		if(($category[0]) && ($category[0]->cat_name != 'Uncategorized' ) ){
-			echo '<div class="artmeta">Project:<a  href="'.get_category_link($category[0]->term_id ).'">'
-				.$category[0]->cat_name.'</a></div>';
-		} ?>
+<?php  // Artworks project taxonomy is covered by Category for better search results
+	$category = get_the_category(); 
 	
-	<?php if (get_the_term_list( $post->ID, 'jyear' ) != null ) { ?>
-		<div class='artmeta'>Year: <?php echo get_the_term_list( $post->ID, 'jyear', '', ', ', '' ); ?></div>
-	<?php } ?>
+	if(($category[0]) && ($category[0]->cat_name != 'Uncategorized' ) ){
+		echo '<div class="artmeta">Project:<a  href="'. get_category_link($category[0]->term_id ) .'">'
+				. $category[0]->cat_name.'</a></div>';
+	} 
+?>
+	
+<?php // Year is a reserved taxonomy name, 
+	if (get_the_term_list( $post->ID, 'jyear' ) != null ) { 
+?>		
+
+<div class='artmeta'>Year: <?php echo get_the_term_list( $post->ID, 'jyear', '', ', ', '' ); ?></div>
+
+<?php 
+	} 
+?>
+
 	<?php if (get_the_term_list( $post->ID, 'dimensions' ) != null ) { ?>
 		<div class='artmeta'>Dimensions: <?php echo get_the_term_list( $post->ID, 'dimensions', '', ', ', '' ); ?></div>
 	<?php } ?>
