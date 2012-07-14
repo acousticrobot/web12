72d98e4676d37000215f33a2acbb8e7533c1f96d
Jonathan Gabel <post@jonathangabel.com>
2012-07-14 07:13:55 -0400

    switching to sass stylesheets using compass

--- home.php
+++ home.php
@@ -1,30 +1,40 @@
+<?php
+/*
+*	Description: Main Template for home page
+*	
+*	CALLED BY: URL
+*	TRIGGER: home: jonathangabel.com/
+*	CALLS TO:	HEADER->home-head, main side nav, loop-carousel, loop-status, FOOTER
+*  	v 2.0	 
+*/
+?>
+
 <?php get_header(); ?>
 	
-	<h2>recent additions:</h2>
+<h2>recent additions:</h2>
 
-	<nav id="mainSideNav">
-		<?php wp_nav_menu(array('menu' => 'Main Side Navigation'))?>
-	</nav> <!-- mainSideNav -->
-	
-	<div id="leftArrow"></div>
-	<div id= "cabinet">
-		<?php get_template_part('loop','carousel');?> 
-	</div><!-- cabinet-->
-	<div id="rightArrow"></div>
-	
-	<section>
-		<aside>
-			<h2><a id="news" href="#" >News</a></h2>
-			<div class="scroll-pane">
-			<?php get_template_part('loop','status');?> 
-			<?php wp_reset_query();	?> 
-			</div>
-		</aside>
-			
-		<div id= "pictureblock">
-			<canvas id="ctx" ></canvas>
-		</div>				
-	</section>
-	
+<nav id="mainSideNav">
+	<?php wp_nav_menu(array('menu' => 'Main Side Navigation'))?>
+</nav> <!-- mainSideNav -->
 
+<div id="leftArrow"></div>
+<div id= "cabinet">
+	<?php get_template_part('loop','carousel');?> 
+</div><!-- cabinet-->
+<div id="rightArrow"></div>
+
+<section>
+	<aside>
+		<h2><a id="news" href="#" >News</a></h2>
+		<div class="scroll-pane">
+		<?php get_template_part('loop','status');?> 
+		<?php wp_reset_query();	?> 
+		</div>
+	</aside>
+		
+	<div id= "pictureblock">
+		<canvas id="ctx" ></canvas>
+	</div>				
+</section>
+	
 <?php get_footer(); ?>
--- inc/childlist.php
+++ inc/childlist.php
@@ -1,16 +1,24 @@
 <?php 
-	/*
-	*	Description: Lists all child pages
-	*	Called by: template-mainset(pr), t47
-	*	v 1.2
-	*/
+/*
+*	Description: Lists all child pages
+*
+*	CALLED BY: 	template-mainset: projects, t47
+*	TRIGGER: 	custom field
+*	CALLS TO:	HEADER->home-head, main side nav, loop-carousel, loop-status, FOOTER
+*  	v 2.0	 
+*/
+
 	
 	$children = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0'); 
 	
-	if ($children) {
-		echo '<div class="link-list">';
-		echo '<ul>' . $children . '</ul>'; 
-		echo '</div>';
- 	}
+	if ($children) : ?>
 
- ?>
+<div class="link-list">
+	<ul>
+		<?php echo $children ?>
+	</ul>
+</div>
+
+<?php 
+	endif; 
+?>
